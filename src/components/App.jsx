4065a8dbb78260be98c4c571423cc43c0074ac5e
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-13 02:13:14 +0300

    strange mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ export const App = () => {
     <div className="App">
       <Header />
       <GroznyShop />
-
     </div>
   );
 }
--- src/components/Content/GroznyShop.jsx
+++ src/components/Content/GroznyShop.jsx
@@ -3,6 +3,7 @@ import { Logo } from './Logo/Logo';
 import styles from './content.module.css'
 import { Search } from './Search/Search'
 import { Services } from './Services/Services'
+import { Recomendation } from './Recomendation/Recomendation'
 
 export const GroznyShop = (props) => {
   return (
@@ -12,6 +13,7 @@ export const GroznyShop = (props) => {
       <Search />
     </div>
       <Services />
+     <Recomendation />
     </>
 
   )
