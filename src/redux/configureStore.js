9a18dd9359d95f4f8ea7eaf990370cf62b30815c
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-18 20:20:50 +0300

    fetch getInfoFromServer commit

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { createLogger } from 'redux-logger/src'
 import thunk from 'redux-thunk'
-
+import { AdvertisementReducer } from './homeAdvertisementReducer/advertisement'
+import { openAdvertisementReducer } from './openAdvertisement/openAdvertisement'
 
 
 const logger = createLogger({
@@ -9,11 +10,9 @@ const logger = createLogger({
   collapsed: true
 })
 
-
 const rootReducer = combineReducers({
-  ////
+  cards: AdvertisementReducer,
+  OpenCards: openAdvertisementReducer
 })
-
-
 
 export const store = createStore(rootReducer, applyMiddleware(thunk, logger))
